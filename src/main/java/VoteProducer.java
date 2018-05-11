c8e2850fa86043da07267bf06d2474f88eba732f
Fabrice Touopi <f.touopi.touopi@accenture.com>
2018-05-11 07:24:18 +0400

    update

--- src/main/java/VoteProducer.java
+++ src/main/java/VoteProducer.java
@@ -3,78 +3,75 @@
  */
 
 //import util.properties packages
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 //import simple producer packages
-import org.apache.kafka.clients.producer.Producer;
-
 //import KafkaProducer packages
-import org.apache.kafka.clients.producer.KafkaProducer;
-
 //import ProducerRecord packages
-import org.apache.kafka.clients.producer.ProducerRecord;
 
-//Create java class named “SimpleProducer�?
+//Create java class named “SimpleProducer�?
 public class VoteProducer {
-    private final static String VOTE_TOPIC="vote-topic";
-     static final String SEPARATOR=";";
+    private final static String VOTE_TOPIC = "vote-topic";
+    static final String SEPARATOR = ";";
 
-    public static void main(String[] args) throws Exception{
-
+    public static void main(String[] args) throws Exception {
         // Check arguments length value
-       /** if(args.length == 0){
-            System.out.println("Enter topic name");
-            return;
-        }*/
+        /** if(args.length == 0){
+         System.out.println("Enter topic name");
+         return;
+         }*/
 
         //Assign topicName to string variable
         //String topicName = args[0].toString();
 
         // create instance for properties to access producer configs
-        Properties props = new Properties();
+        Properties properties = new Properties();
 
         //Assign localhost id
-       // props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
-       props.put("bootstrap.servers", "localhost:9092");
+        // properties.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
+        properties.put("bootstrap.servers", "localhost:9092");
 
         //Set acknowledgements for producer requests.
-        props.put("acks", "all");
+        properties.put("acks", "all");
 
-                //If the request fails, the producer can automatically retry,
-                props.put("retries", 0);
+        //If the request fails, the producer can automatically retry,
+        properties.put("retries", 0);
 
         //Specify buffer size in config
-        props.put("batch.size", 16384);
+        properties.put("batch.size", 16384);
 
         //Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
+        properties.put("linger.ms", 1);
 
         //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        props.put("buffer.memory", 33554432);
+        properties.put("buffer.memory", 33554432);
 
-        props.put("key.serializer",
+        properties.put("key.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
 
-        props.put("value.serializer",
+        properties.put("value.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
 
-        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-           
-            for (long i=0;i<1500;i++) {
+        try (Producer<String, String> producer = new KafkaProducer<>(properties)) {
+
+            for (long i = 0; i < 15000000; i++) {
                 int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
-                final String msg= new StringBuilder().append(randomNum).append(SEPARATOR).append(UUID.randomUUID().toString()).toString();
+                final String msg = new StringBuilder().append(randomNum).append(SEPARATOR).append(UUID.randomUUID().toString()).toString();
                 producer.send(new ProducerRecord<>(VOTE_TOPIC,
-                        UUID.randomUUID().toString(),msg));
+                        UUID.randomUUID().toString(), msg));
                 System.out.println(msg);
                 if (i % 10 == 0) {
                     Thread.sleep(1000);
                 }
             }
-            
+
             System.out.println("Vote sent successfully");
         }
     }
